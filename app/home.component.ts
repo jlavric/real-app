0da7af1a03969050ad48a7c478355c55802b442a
Lavric Janez <Lavric_Janez@rtvslo.si>
2016-10-13 07:37:49 +0200

    two way binding sample

--- app/home.component.ts
+++ app/home.component.ts
@@ -2,7 +2,11 @@ import {Component,OnInit} from 'angular2/core';
 
 @Component({
     
-    template: '<h1>Home</h1>'
+    template: `
+        <h1>Home</h1> 
+            <input type="text" [value]="title" (input)="title = $event.target.value" />
+            Preview: {{title}}
+            `
 
 })
 export class HomeComponent  {
